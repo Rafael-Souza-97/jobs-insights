85202dc48ed20487e835004e421ecfbe452c23a0
Rafael Souza <souza_rafael.97@outlook.com>
2023-03-24 16:29:42 -0300

    feat: req 7

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -11,18 +11,8 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    all_filtered_industries = []
+    for job in jobs:
+        if job['industry'] == industry:
+            all_filtered_industries.append(job)
+    return all_filtered_industries
