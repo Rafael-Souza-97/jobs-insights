86a74b3659d90523e180c3d248af9af21622960e
Rafael Souza <souza_rafael.97@outlook.com>
2023-03-24 17:07:54 -0300

    feat: req 8

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -27,29 +27,26 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    try:
+        min_salary = int(job['min_salary'])
+        max_salary = int(job['max_salary'])
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    except KeyError:
+        raise ValueError("'min_salary' e 'max_salary' são obrigatórios.")
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    except TypeError:
+        raise ValueError("'min_salary' e 'max_salary' devem ser números.")
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        int_salary = int(salary)
+
+    except TypeError:
+        raise ValueError("O salário deve ser um número.")
+
+    if min_salary > max_salary:
+        raise ValueError("'max_salary' deve ser maior que 'min_salary'.")
+
+    return max_salary >= int_salary >= min_salary
 
 
 def filter_by_salary_range(
