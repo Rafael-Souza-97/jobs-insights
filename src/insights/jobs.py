d76ba2e4ee611533bb46b858c5554ce255b95b53
Rafael Souza <souza_rafael.97@outlook.com>
2023-03-24 15:31:28 -0300

    feat: req 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -14,21 +14,11 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    job_types = set()
+    for row in read(path):
+        if row['job_type']:
+            job_types.add(row['job_type'])
+    return job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
