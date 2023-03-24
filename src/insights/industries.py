630a0a52e6b712bbbe5c27b1f6608daba4aa9919
Rafael Souza <souza_rafael.97@outlook.com>
2023-03-24 15:44:09 -0300

    feat: req 3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,13 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    industries = set()
+    for row in read(path):
+        if row['industry']:
+            industries.add(row['industry'])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
